acfd1b1972f9a03753eef1b39656aa163b0bfd94
Josh Kuhar <jdkuhar@gmail.com>
2016-06-18 16:16:03 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+"use strict";
+
+$(function(){
+$('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+
+});
+
+function getRequest(searchTerm){
+  var params = {
+    s: searchTerm,
+    r: 'json'
+  };
+  var url = 'http://www.omdbapi.com';
+
+  $.getJSON(url, params, function(data){
+    showResults(data.Search);
+  });
+}
+
+function showResults(results){
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
+}
+
+
+
+
+/*
+
+
+$('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+
+
+*/
