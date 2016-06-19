986f5d0b8a29048118a4113ba2beb5f41d44260f
Josh Kuhar <jdkuhar@gmail.com>
2016-06-19 13:23:44 -0400

    update

--- app.js
+++ app.js
@@ -1,21 +1,94 @@
-
 "use strict";
 
+
+var params = {
+  q: "Elvis", 
+  part: 'snippet', 
+  key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"
+};
+
+var url = "https://www.googleapis.com/youtube/v3/search";
+
+$.getJSON(url, params, function(data) {
+
+  for (var y = 0; y < 5; y++) {
+    var line = data.items[y].snippet.channelTitle;
+    console.log(line);
+    $('#search-results').append('<p>Test Case 1: ' + line + '</p>');
+    
+  }
+  //$('#search-results').append('<p>Test Case 2:' + line + '</p>');
+  //$('#search-results').html('<p>' + line + '</p>');
+  //var results = $.each(data.items, function(index,value){
+    //$('#search-results').html('<p>' + results + '</p>');
+  //});
+  //console.log(data.items);
+  });
+
+
+
+
+/*
+//For exploring object
+$.getJSON("https://www.googleapis.com/youtube/v3/search", {q: "Elvis", part: 'snippet', key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"}, function(data) {
+  console.log(data);
+  });
+
+
+Key: AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk
+
 $(function(){
-$('#search-term').submit(function(event){
+  $('#search-term').submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
     getRequest(searchTerm);
   });
-
 });
 
 function getRequest(searchTerm){
   var params = {
-    s: searchTerm,
-    r: 'json'
+    q: q,
+    part: 'snippet',
+    key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"
   };
-  var url = 'http://www.omdbapi.com';
+  var url = 'https://www.googleapis.com/youtube/v3/search';
+
+  $.getJSON(url, params, function(data){
+    showResults(data);
+  });
+}
+
+function showResults(results){
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + results + '</p>';
+    console.log(results);
+  });
+  $('#search-results').html(html);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getRequest(searchTerm){
+  var params = {
+    part: "snippet",
+    key: 'AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk',
+    q: searchTerm
+  };
+  var url = 'https://www.googleapis.com/youtube/v3/search';
 
   $.getJSON(url, params, function(data){
     showResults(data.Search);
@@ -25,17 +98,49 @@ function getRequest(searchTerm){
 function showResults(results){
   var html = "";
   $.each(results, function(index,value){
-    html += '<p>' + value.Title + '</p>';
-    console.log(value.Title);
+    html += '<p>' + results + '</p>';
+    console.log(response.result);
   });
   $('#search-results').html(html);
 }
-
+*/
 
 
 
 /*
+$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    console.log(data);
+  });
 
+$.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk&q=elvis', function(data){
+    console.log(data);
+  });
+
+$.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk&q=elvis', function(data){
+    var myData = data.items;
+      $.each(myData, function(index, value){
+      console.log(myData[2]);
+    });
+  });
+
+3
+$.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk&q=elvis', function(data){
+    var myData = data.Search;
+    $.each(myData, function(index, value){
+      console.log(value.Title);
+    });
+  });
+
+2
+$.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk&q=elvis', function(data){
+  var myData = data.Search[0].Title;
+  console.log(myData);
+});
+
+1
+$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    console.log(data);
+  });
 
 $('#search-term').submit(function(event){
     event.preventDefault();
