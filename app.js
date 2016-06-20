c574515d496d2b290c89f893e988d74fc8a1ce0b
Josh Kuhar <jdkuhar@gmail.com>
2016-06-20 15:10:44 -0400

    update

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 "use strict";
 
-$('#search-term').submit(function(event){
+$('#search-term').submit(function(event) {
     event.preventDefault();
     var searchTerm = $('#query').val();
     getRequest(searchTerm);
-  });
+});
 
-function getRequest(searchTerm){
+function getRequest(searchTerm) {
     var params = {
-    q: searchTerm, 
-    part: 'snippet', 
-    key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"
+        q: searchTerm,
+        part: 'snippet',
+        key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"
     };
     var url = "https://www.googleapis.com/youtube/v3/search";
     $.getJSON(url, params, function(data) {
-      for (var y = 0; y < data.items.length; y++) {
-      var line = data.items[y].snippet.thumbnails.default.url;
-      showResults(line);  
-      }
-    }) 
-  }
+        console.log(data);
+        var results = data.items;
 
-function showResults(results) {
-  $('#search-results').append('<p><img src="' + results + '"></p>');
+        for (var i = 0; i < results.length; i++) {
+            showResults(results[i]);
+        }
+
+
+    })
+}
+
+function showResults(movie) {
+    console.log(movie);
+    var m = movie.snippet;
+    $('#search-results').append('<div>' + '<h1>' + m.title + '</h1>' + '<p>' + m.description + '</p><iframe src="' + 'https://www.youtube.com/embed/' + movie.id.videoId + '"></iframe>' + '</div>')
+
 }
 
 
@@ -55,6 +62,12 @@ function showResults(results){
   });
   $('#search-results').html(html);
 }
+<a href="https://www.youtube.com/"></a>
+watch?v=vGJTaP6anOU
+
+
+
+https://www.youtube.com/watch?v=vGJTaP6anOU
 */
 
 
