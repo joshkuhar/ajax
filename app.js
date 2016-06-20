e8e2e0140703de58f489976ccc551b3697523357
Josh Kuhar <jdkuhar@gmail.com>
2016-06-20 11:43:33 -0400

    update

--- app.js
+++ app.js
@@ -1,42 +1,29 @@
 "use strict";
 
-
-var params = {
-  q: "Elvis", 
-  part: 'snippet', 
-  key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"
-};
-
-var url = "https://www.googleapis.com/youtube/v3/search";
-
-$.getJSON(url, params, function(data) {
-
-    //for (var y = 0; y < 5; y++) {
-      //var line = data.items[y].snippet.channelTitle;
-      //console.log(line);
-      //$('#search-results').append('<p>Test Case 1: ' + line + '</p>');  
-    //}
-    $('#search-results').append('<p>Test Case 2:' + data + '</p>');
-    //$('#search-results').html('<p>' + line + '</p>');
-    //var results = $.each(data.items, function(index,value){
-    //$('#search-results').html('<p>' + results + '</p>');
-    //});
-    console.log(data);
-
-    for(var item in data) {
-      $('#search-results').append('<p>' + item + ": " + data[item] + '</p>');
-
-    }
-
-
-
-    //});
-
-
-
+$('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
   });
 
+function getRequest(searchTerm){
+    var params = {
+    q: searchTerm, 
+    part: 'snippet', 
+    key: "AIzaSyC35BPW7bRtEl8Pb47Wr-7FtxEc3XUBYtk"
+    };
+    var url = "https://www.googleapis.com/youtube/v3/search";
+    $.getJSON(url, params, function(data) {
+      for (var y = 0; y < data.items.length; y++) {
+      var line = data.items[y].snippet.thumbnails.default.url;
+      showResults(line);  
+      }
+    }) 
+  }
 
+function showResults(results) {
+  $('#search-results').append('<p><img src="' + results + '"></p>');
+}
 
 
 /*
